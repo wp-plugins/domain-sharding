f6f85d30b8b74ba2683b333492916ae493e37ff8
SultanICQ <SultanICQ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-13 18:01:25 +0000

    = 1.0.4 = * Added some tests to help us check for the validity of the desired subdomains. They are located in the Settings page.
    git-svn-id: https://plugins.svn.wordpress.org/domain-sharding/trunk@857361 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- domain-sharding.php
+++ domain-sharding.php
@@ -4,12 +4,13 @@ Plugin Name: Domain Sharding
 Plugin URI: http://www.seocom.es
 Description: This plugin allows us to change the root domain of images and stylesheets that currently are inside the actual domain and then use a domain sharding structure.
 Author: David Garcia
-Version: 1.0.3
+Version: 1.0.4
 */
 
 class domain_sharding
 {
 	var $_slug;
+	var $_slugfile;
 	var $_hook;
 
 	var $main_domain_schema;
@@ -31,6 +32,7 @@ class domain_sharding
 	function __construct()
 	{
 		$this->_slug = basename(dirname(__FILE__));
+		$this->_slugfile = $this->_slug . '.php';
 		$this->_hook = basename(__FILE__);
 
 		$this->set_home();
@@ -40,7 +42,7 @@ class domain_sharding
 		{
 			add_action('admin_menu', array(&$this, 'admin_menu') );
 			add_action('admin_notices', array( $this, 'show_messages' ) );
-
+			add_filter('plugin_action_links', array( $this, 'plugin_action_links' ), 10, 2 );
 			return;
 		}
 
@@ -48,6 +50,15 @@ class domain_sharding
 		{
 			add_action('init', array(&$this,'process'), 100);
 		}
+	}
+
+	function plugin_action_links( $links, $file )
+	{
+		if ( $file == plugin_basename( dirname(__FILE__). '/' . $this->_slugfile ) ) {
+			$links[] = '<a href="' . admin_url( 'admin.php?page=' . $this->_slugfile ) . '">'.__( 'Settings' ).'</a>';
+		}
+
+		return $links;
 	}
 
 	function set_home()
@@ -66,11 +77,18 @@ class domain_sharding
 
 	function set_ds()
 	{
+		$this->ds_domain='';
+		$this->ds_max=0;
+		$this->ds_exclusions='';
+		$this->valid = false;
+
 		$option = get_option('domain_sharding_config');
 		if ( !is_array($option) )
 		{
+			echo 2;die;
 			return;
 		}
+
 		$this->ds_domain = trim($option['domain']);
 		$this->ds_max = intval($option['max']);
 		$this->ds_exclusions = explode("\n", $option['exclusions']);
@@ -96,9 +114,88 @@ class domain_sharding
 		if ( isset($_POST['domain_sharding']) )
 		{
 			update_option( 'domain_sharding_config', $_POST['domain_sharding'] );
+			$this->set_ds();
 			print '<div id="message" class="updated fade"><p><strong>'.__('Options updated.', $this->_slug ).'</strong> <a href="'.get_bloginfo('url').'">'.__('View site', $this->_slug ) . ' &raquo;</a></p></div>';
 		}
 		$option = get_option('domain_sharding_config');
+
+		$verify_result = '';
+		if ( isset($_POST['domain_sharding_check']) )
+		{
+			if ( function_exists('gethostbyname') )
+			{
+				if ( ! (empty($option['domain']) || empty($option['max']) ) )
+				{
+					$plugin_url = plugin_dir_url(__FILE__);
+
+					// Get the contents of the test file.
+					$main_check_file = 'domain.check.valid.txt';
+					$main_check_value = wp_remote_get($plugin_url . $main_check_file );
+					if ( is_array( $main_check_value ) && $main_check_value['response']['code']=='200' )
+					{
+						$main_check_value = $main_check_value['body'];
+					} else {
+						$main_check_value = '';
+					}
+
+					if ( !empty($main_check_value) )
+					{
+						$main_domain = parse_url( home_url(), PHP_URL_HOST );
+						$main_domain_ip = gethostbyname( $main_domain );
+
+						$verify_result .= '<p>'.__('Main domain ip',$this->_slug).': <strong>'.$main_domain_ip.'</strong></p>';
+						$verify_result .= '<p>'.__('Verifying subdomains',$this->_slug).':</p>';
+
+						for($x=1;$x<=intval($option['max']);$x++)
+						{
+
+							$ok = true;
+
+							$domain = $this->ds_build_domain( $this->ds_concat_subdomain( $this->ds_domain, $x ), false );
+							$verify_result .= $domain . ': ';
+
+							$domain_ip = gethostbyname( $domain );
+							if ( $domain_ip != $main_domain_ip )
+							{
+								$verify_result .= ' ' . sprintf( __('The subdomain ip %s differs from the ip of main domain.',$this->_slug), $domain_ip );
+								$ok = false;
+							}
+
+							if ( $ok )
+							{
+								$domain_plugin_url = str_replace($main_domain, $domain, $plugin_url );
+								$check_value = wp_remote_get($plugin_url . $main_check_file );
+								if ( is_array( $check_value ) && $check_value['response']['code']=='200' )
+								{
+									$check_value = $check_value['body'];
+								} else {
+									$check_value = '';
+								}
+								if ( $check_value != $main_check_value )
+								{
+									$verify_result .= ' <strong style="color:#ACA700">'.__('Subdomain does not seem to point to the same physical directory of the main domain', $this->_slug).'.</strong>';	
+									$ok = false;
+								}
+							}
+
+							if ( $ok )
+							{
+								$verify_result .= ' <strong style="color:#4DD25C">'.__('Is valid', $this->_slug).'.</strong>';
+							} else {
+								$verify_result .= ' <strong style="color:#F00;text-decoration:uppercase;">'.__('Is not valid', $this->_slug).'!!!</strong>';
+							}
+
+							$verify_result .= '<br/>';
+
+						}
+					} else {
+						$verify_result .= ' <strong style="color:#F00;text-decoration:uppercase;">'.__('The file used to check the subdomain configuration is missing. Try reinstalling the plugin.', $this->_slug).'</strong>';
+					}
+				}
+			} else {
+				$verify_result .= ' <strong style="color:#F00;text-decoration:uppercase;">'.__('Your PHP installation does not allow us to use the gethostbyname function.', $this->_slug).'</strong>';
+			}
+		}
 	
 		print '
 		<div class="wrap">
@@ -125,6 +222,13 @@ class domain_sharding
 		<p>'.__('The final domain will follow the structure', $this->_slug ).' http://[domain][1-MaxDomain].[basedomain]</p>
 		<p>'.__('<strong>NOTE:</strong> You\'ll need to manually create the new A records for the subdomains in your DNS panel. They should have the same ip address of your main domain.').'</p>
 		<p class="submit"><input type="submit" value="Submit &raquo;" class="button button-primary"/></p>
+		</form>
+
+		<p>&nbsp;</p>
+
+		<form method="post" action="http://'.$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'].'">
+		<p class="submit"><input type="submit" value="'.__('Verify subdomains', $this->_slug).'" class="button button-primary" name="domain_sharding_check" /></p>
+		'.$verify_result.'
 		</form>
 
 		</div>
@@ -202,21 +306,37 @@ class domain_sharding
 		return $buffer;
 	}
 
+	function ds_concat_subdomain( $sub, $number )
+	{
+		return $sub.$number;
+	}
+
 	function ds_calc_subdomain( $value, $sub = 'cdn' )
 	{
 		$number = abs( ( crc32( $value ) % $this->ds_max ) ) + 1;
 		
-		return $sub.$number;
+		return $this->ds_concat_subdomain( $sub, $number );
 	}
+
+	function ds_build_domain( $subdomain, $use_schema = true )
+	{
+		$domain = $subdomain .'.'.$this->main_domain;
+		if ( $use_schema )
+		{
+			$domain = $this->main_domain_schema . $domain;
+		}
+		return $domain;
+	}
+
 	function ds_parse( $value )
 	{	
 		if ( substr( $value, 0, $this->home_len ) != $this->home )
 		{
 			return $value;
 		}
+		$subdomain = $this->ds_calc_subdomain( $value, $this->ds_domain );
 
-		$domain = $this->main_domain_schema.$this->ds_calc_subdomain( $value, $this->ds_domain ).'.'.$this->main_domain;
-
+		$domain = $this->ds_build_domain($subdomain);
 		$domain = $domain . substr( $value, $this->home_len );
 
 		return $domain;
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: sultanicq
 Tags: cdn, wpo, domain sharding, speed, optimization
 Requires at least: 2.8
 Tested up to: 3.8.1
-Stable tag: 1.0.3
+Stable tag: 1.0.4
 
 This plugin modify the url of the images to speed up the page browsing.
 
@@ -24,6 +24,9 @@ Visit the <a href="http://www.seocom.es/">Seocom website</a> for more informatio
 3. It's done. Easy, isn't it?
 
 == Changelog ==
+
+= 1.0.4 =
+* Added some tests to help us check for the validity of the desired subdomains. They are located in the Settings page.
 
 = 1.0.3 =
 * BugFix. The plugin now works with blogs not installed in the root domain.
