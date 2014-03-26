51793e7b75e2b1472b99ab64067be4ff7a5e8d2e
SultanICQ <SultanICQ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-26 12:24:30 +0000

    = 1.1.4 = * Automagically try to guess the right value for the old domain variable.
    git-svn-id: https://plugins.svn.wordpress.org/domain-sharding/trunk@882291 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- domain-sharding.php
+++ domain-sharding.php
@@ -4,7 +4,7 @@ Plugin Name: Domain Sharding
 Plugin URI: http://www.seocom.es
 Description: This plugin allows us to change the root domain of images and stylesheets that currently are inside the actual domain and then use a domain sharding structure.
 Author: David Garcia
-Version: 1.1.3
+Version: 1.1.4
 */
 
 class domain_sharding
@@ -101,9 +101,15 @@ class domain_sharding
 		if ( substr_count($option['domain'], '.')<=1 )
 		{
 			// We were still using an old version.
-			$option['domain']='';
+			$host_parsed = parse_url($this->home);
+			$host = explode('.', $host_parsed['host'] );
+			$host[0]=$this->ds_domain.'#';
+			$host = implode('.', $host);
+			$host = $host_parsed['scheme'].'://'.$host;
+
+			$this->ds_domain=$host;
+			$option['domain']=$host;
 			update_option('domain_sharding_config', $option);
-			$this->ds_domain='';
 		}
 
 		$this->ds_max = intval($option['max']);
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: sultanicq
 Tags: cdn, wpo, domain sharding, speed, optimization
 Requires at least: 2.8
 Tested up to: 3.8.1
-Stable tag: 1.1.3
+Stable tag: 1.1.4
 
 This plugin modify the url of the images to speed up the page browsing.
 
@@ -36,6 +36,9 @@ NOTE: If you have trouble accessing images using the new address because Wordpre
 NOTE: You'll need to manually create the new A records for the subdomains in your DNS panel. They should have the same ip address of your main domain.
 
 == Changelog ==
+
+= 1.1.4 =
+* Automagically try to guess the right value for the old domain variable.
 
 = 1.1.3 =
 * Fixed re-activation detection when upgrading plugin.
